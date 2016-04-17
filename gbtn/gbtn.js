c38e813f6bad350313b0d1fa41e82e5fdab37b41
simalexan <alexander.simovic@gmail.com>
2016-04-17 18:08:27 +0200

    adding move-right and left animations

--- gbtn/gbtn.js
+++ gbtn/gbtn.js
@@ -689,18 +689,25 @@ var GiftButton = (function(window, undefined) {
       GiftButton.$('.gButton-slider > .next').removeClass('active');
 
     } else if (GiftButton.promotions.length > 3){
-      GiftButton.$('.gButton-slider > .prev').removeClass('prev').addClass('current active');
-      GiftButton.$('.gButton-slider > .current').not('.active').removeClass('current').addClass('next active');
-      GiftButton.$('.gButton-slider > .current').removeClass('active');
-      GiftButton.$('.gButton-slider > .next').not('.active').removeClass('next').addClass('inactive');
-      GiftButton.$('.gButton-slider > .next').removeClass('active');
+
+      GiftButton.$('.gButton-slider .next').addClass('move-right');
+      GiftButton.$('.gButton-slider .current').addClass('move-right');
+      setTimeout(function(){
+        GiftButton.$('.gButton-slider > .prev').removeClass('prev').addClass('current active');
+        GiftButton.$('.gButton-slider > .current').not('.active').removeClass('current').addClass('next active');
+        GiftButton.$('.gButton-slider > .current').removeClass('active');
+        GiftButton.$('.gButton-slider > .next').not('.active').removeClass('next').addClass('inactive');
+        GiftButton.$('.gButton-slider > .next').removeClass('active');
 
 
-      if (GiftButton.$('.gButton-slider > .current').next('li.inactive').length) {
-        GiftButton.$('.gButton-slider > .current').next('li.inactive').removeClass('inactive').addClass('prev');
-      } else {
-        GiftButton.$('.gButton-slider > li.inactive:first').removeClass('inactive').addClass('prev');
-      }
+        if (GiftButton.$('.gButton-slider > .current').next('li.inactive').length) {
+          GiftButton.$('.gButton-slider > .current').next('li.inactive').removeClass('inactive').addClass('prev');
+        } else {
+          GiftButton.$('.gButton-slider > li.inactive:first').removeClass('inactive').addClass('prev');
+        }
+      }, 700);
+
+
 
     }
 
@@ -769,19 +776,23 @@ var GiftButton = (function(window, undefined) {
       GiftButton.$('.gButton-slider > .prev').removeClass('active');
 
     } else if (GiftButton.promotions.length > 3){
+      GiftButton.$('.gButton-slider .prev').addClass('move-left');
+      GiftButton.$('.gButton-slider .current').addClass('move-left');
 
-      GiftButton.$('.gButton-slider > .next').removeClass('next').addClass('current active');
-      GiftButton.$('.gButton-slider > .current').not('.active').removeClass('current').addClass('prev active');
-      GiftButton.$('.gButton-slider > .current').removeClass('active');
-      GiftButton.$('.gButton-slider > .prev').not('.active').removeClass('prev').addClass('inactive');
-      GiftButton.$('.gButton-slider > .prev').removeClass('active');
+      setTimeout(function(){
+        GiftButton.$('.gButton-slider > .next').removeClass('next').addClass('current active');
+        GiftButton.$('.gButton-slider > .current').not('.active').removeClass('current').addClass('prev active');
+        GiftButton.$('.gButton-slider > .current').removeClass('active');
+        GiftButton.$('.gButton-slider > .prev').not('.active').removeClass('prev').addClass('inactive');
+        GiftButton.$('.gButton-slider > .prev').removeClass('active');
 
 
-      if (GiftButton.$('.gButton-slider > .current').prev('li.inactive').length) {
-        GiftButton.$('.gButton-slider > .current').prev('li.inactive').removeClass('inactive').addClass('next');
-      } else {
-        GiftButton.$('.gButton-slider > li.inactive:last').removeClass('inactive').addClass('next');
-      }
+        if (GiftButton.$('.gButton-slider > .current').prev('li.inactive').length) {
+          GiftButton.$('.gButton-slider > .current').prev('li.inactive').removeClass('inactive').addClass('next');
+        } else {
+          GiftButton.$('.gButton-slider > li.inactive:last').removeClass('inactive').addClass('next');
+        }
+      }, 700);
     }
 
 
